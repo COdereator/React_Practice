0641be3fc7cbc73d9d3645548c9f0b1f67063892
@priyanshu-vish <priyanshuvsk20@gmail.com>
2024-09-04 10:38:33 +0530

    Add files via upload
    Question

---
+++ src/Question.jsx
@@ -0,0 +1,32 @@
+export const datas = [
+    {
+        "id": 1,
+        "question": "Capital of France?",
+        "short_answer": "The capital of France is Paris, which is known for its rich history, culture, and landmarks such as the Eiffel Tower and the Louvre Museum. It is also a major center for art, fashion, and gastronomy."
+    },
+    {
+        "id": 2,
+        "question": "Author of 'To Kill a Mockingbird'?",
+        "short_answer": "The novel 'To Kill a Mockingbird' was written by Harper Lee. Published in 1960, it is a classic of modern American literature, exploring themes of racial injustice and moral growth through the eyes of a young girl in the Deep South."
+    },
+    {
+        "id": 3,
+        "question": "Boiling point of water?",
+        "short_answer": "Water boils at 100 degrees Celsius (212 degrees Fahrenheit) at standard atmospheric pressure (1 atmosphere). This boiling point can change with variations in altitude and pressure."
+    },
+    {
+        "id": 4,
+        "question": "Chemical symbol for gold?",
+        "short_answer": "The chemical symbol for gold is Au. It comes from the Latin word 'aurum.' Gold is a precious metal known for its luster, malleability, and conductivity, and has been used for jewelry, currency, and various industrial applications."
+    },
+    {
+        "id": 5,
+        "question": "Number of continents?",
+        "short_answer": "There are seven continents on Earth: Africa, Antarctica, Asia, Europe, North America, Australia (Oceania), and South America. These continents vary widely in terms of size, climate, and biodiversity."
+    },
+    {
+        "id": 6,
+        "question": "Red Planet?",
+        "short_answer": "The Red Planet is Mars. It is called this due to its reddish appearance, which is caused by iron oxide (rust) on its surface. Mars has the largest volcano and canyon in the solar system and is a primary target in the search for signs of past life."
+    }
+]
